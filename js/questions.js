e1f3e947747250313b6e3de934bf4646ce947abb
viakeegan <viakeegan@gmail.com>
2021-12-14 11:52:51 -0500

    add quiz questions

---
+++ js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      question: "In which movie did Harry meet his Godfather, Sirius Black?",
+      choices: ["Sorcerer's Stone", "Chamber of Secrets", "Prisoner of Azkaban", "Goblet of Fire"],
+      answer: "Prisoner of Azkaban"
+    },
+    {
+      question: "Who is Tom Riddle?",
+      choices: ["Dumbledore's Brother", "Lord Voldermort", "Professor Lupin", "Firenze"],
+      answer: "Lord Voldermort"
+    },
+    {
+        question: "Which Hogwarts House is the bravest?",
+        choices: ["Slytherin", "Hufflepuff", "Ravenclaw", "Gryffindor"],
+        answer: "Gryfindor"
+    },
+    {
+        question: "What is the last horcrux?",
+        choices: ["Tom Riddle's Diary", "Ravenclaw's Diadem", "Nagini the Snake", "Harry Potter"],
+        answer: "Harry Potter"
+    },
+    {
+        question: "Who killed Professor Dumbledore?",
+        choices: ["Professor Snape", "Malfoy", "Bellatrix Lestrange", "Dementors"],
+        answer: "Professor Snape"
+    }
+]
